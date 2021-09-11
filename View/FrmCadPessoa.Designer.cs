411c4450008f9ed41e10e9bf02a52fe1f5a0a17d
Caiofran <caio.ricciardi@soulasalle.com.br>
2021-09-11 16:13:48 -0300

    update

--- View/FrmCadPessoa.Designer.cs
+++ View/FrmCadPessoa.Designer.cs
@@ -29,12 +29,205 @@ namespace View
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.gpbDadosPrincipais = new System.Windows.Forms.GroupBox();
+            this.gpbEndereco = new System.Windows.Forms.GroupBox();
+            this.gpbDadosAdicionais = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.maskedTextBox3 = new System.Windows.Forms.MaskedTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.gpbDadosPrincipais.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gpbDadosPrincipais
+            // 
+            this.gpbDadosPrincipais.Controls.Add(this.textBox3);
+            this.gpbDadosPrincipais.Controls.Add(this.label6);
+            this.gpbDadosPrincipais.Controls.Add(this.maskedTextBox3);
+            this.gpbDadosPrincipais.Controls.Add(this.label5);
+            this.gpbDadosPrincipais.Controls.Add(this.label4);
+            this.gpbDadosPrincipais.Controls.Add(this.label3);
+            this.gpbDadosPrincipais.Controls.Add(this.label2);
+            this.gpbDadosPrincipais.Controls.Add(this.label1);
+            this.gpbDadosPrincipais.Controls.Add(this.maskedTextBox2);
+            this.gpbDadosPrincipais.Controls.Add(this.maskedTextBox1);
+            this.gpbDadosPrincipais.Controls.Add(this.textBox2);
+            this.gpbDadosPrincipais.Controls.Add(this.textBox1);
+            this.gpbDadosPrincipais.Controls.Add(this.pictureBox1);
+            this.gpbDadosPrincipais.Location = new System.Drawing.Point(26, 37);
+            this.gpbDadosPrincipais.Name = "gpbDadosPrincipais";
+            this.gpbDadosPrincipais.Size = new System.Drawing.Size(564, 221);
+            this.gpbDadosPrincipais.TabIndex = 0;
+            this.gpbDadosPrincipais.TabStop = false;
+            this.gpbDadosPrincipais.Text = "Dados Principais";
+            // 
+            // gpbEndereco
+            // 
+            this.gpbEndereco.Location = new System.Drawing.Point(26, 270);
+            this.gpbEndereco.Name = "gpbEndereco";
+            this.gpbEndereco.Size = new System.Drawing.Size(564, 221);
+            this.gpbEndereco.TabIndex = 1;
+            this.gpbEndereco.TabStop = false;
+            this.gpbEndereco.Text = "Endereço";
+            // 
+            // gpbDadosAdicionais
+            // 
+            this.gpbDadosAdicionais.Location = new System.Drawing.Point(26, 502);
+            this.gpbDadosAdicionais.Name = "gpbDadosAdicionais";
+            this.gpbDadosAdicionais.Size = new System.Drawing.Size(564, 221);
+            this.gpbDadosAdicionais.TabIndex = 2;
+            this.gpbDadosAdicionais.TabStop = false;
+            this.gpbDadosAdicionais.Text = "Dados Adicionais";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(17, 46);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(114, 144);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(176, 114);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(150, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(176, 168);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(150, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(384, 59);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(150, 22);
+            this.maskedTextBox1.TabIndex = 3;
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Location = new System.Drawing.Point(384, 168);
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(150, 22);
+            this.maskedTextBox2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(176, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "CPF:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(176, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Nome:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(176, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Profissão:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(381, 36);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Tel:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(381, 94);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Email:";
+            // 
+            // maskedTextBox3
+            // 
+            this.maskedTextBox3.Location = new System.Drawing.Point(176, 59);
+            this.maskedTextBox3.Name = "maskedTextBox3";
+            this.maskedTextBox3.Size = new System.Drawing.Size(150, 22);
+            this.maskedTextBox3.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(381, 148);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(140, 17);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Data de Nascimento:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(384, 114);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(150, 22);
+            this.textBox3.TabIndex = 14;
+            // 
+            // FrmCadPessoa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FrmCadPessoa";
+            this.ClientSize = new System.Drawing.Size(615, 749);
+            this.Controls.Add(this.gpbDadosAdicionais);
+            this.Controls.Add(this.gpbEndereco);
+            this.Controls.Add(this.gpbDadosPrincipais);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "FrmCadPessoa";
+            this.Text = "Cadastro de Usuários";
+            this.gpbDadosPrincipais.ResumeLayout(false);
+            this.gpbDadosPrincipais.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox gpbDadosPrincipais;
+        private System.Windows.Forms.GroupBox gpbEndereco;
+        private System.Windows.Forms.GroupBox gpbDadosAdicionais;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
